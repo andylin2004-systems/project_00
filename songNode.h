9e979cf888d54267430930d878cade0bf93fea17
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-21 20:31:42 -0400

    made header for songnode

---
+++ songNode.h
@@ -0,0 +1,25 @@
+struct songNode{
+    char name[100];
+    char artist[100];
+    struct songNode *next;
+};
+
+int checkIfShouldPlaceAlphabet(struct songNode *prev, struct songNode *toInsert, struct songNode *next);
+
+struct songNode* addToFront(struct songNode* front, struct songNode* toAdd);
+
+void addBasedOnAlphabet(struct songNode* front, struct songNode* toInsert);
+
+int checkIfShouldPlaceAlphabet(struct songNode* prev, struct songNode* toInsert, struct songNode* next);
+
+void print_node(struct songNode* node);
+
+void print_list(struct songNode* front);
+
+struct songNode* findNodeByNameAndArtist(struct songNode* front, char name[100], char artist[100]);
+
+struct songNode * findNodeByArtist(struct songNode * front, char artist[100]);
+
+struct songNode * returnRandomNode(struct songNode * front);
+
+struct songNode * removeNodeByNameAndArtist(struct songNode * front, char name[100], char artist[100]);
