e1ee97949117a7a0c19072306b046042cf2d8e5a
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-21 20:31:35 -0400

    started making mytunes

--- myTunes.c
+++ myTunes.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
+#include "songNode.h"
 
-struct songNode library[27];
+struct songNode* library[27];
+
+void allocateSongs(){
+    int i;
+    for (i = 0; i < 27; i++){
+        library[i] = malloc(sizeof(struct songNode));
+    }
+}
+
+void addSong(struct songNode* song){
+    
+}
