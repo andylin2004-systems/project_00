2f60ec2d70b40b475e2a972d414c345c53a7340d
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-23 15:12:01 -0400

    modified main test func to accomdadate changes

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 #include "songNode.h"
 
 int main(){
-    allocateSongs();
-    addSong(createNode("thunderstruck", "ac/dc"));
-    addSong(createNode("alive", "pearl jam"));
-    addSong(createNode("even flow", "pearl jam"));
-    addSong(createNode("yellow ledbetter", "pearl jam"));
-    addSong(createNode("time", "pink floyd"));
-    addSong(createNode("paranoid android", "radiohead"));
-    addSong(createNode("street spirit (fade out)", "radiohead"));
+    struct songNode *library = allocateSongs();
+    addSong(library, createNode("thunderstruck", "ac/dc"));
+    addSong(library, createNode("alive", "pearl jam"));
+    addSong(library, createNode("even flow", "pearl jam"));
+    addSong(library, createNode("yellow ledbetter", "pearl jam"));
+    addSong(library, createNode("time", "pink floyd"));
+    addSong(library, createNode("paranoid android", "radiohead"));
+    addSong(library, createNode("street spirit (fade out)", "radiohead"));
     printf("LINKED LIST TEST\n\nTESTING PRINT_LIST");
-    printLibrary();
-    printf("fucc");
-    print_node(library[0]);
+    printLibrary(library);
+    print_node(&library[0]);
 }
